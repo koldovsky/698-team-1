ecdc360c38a54281ea6672942f8c5a4187f9ac52
maria-bilo <marichkabilo@gmail.com>
2022-06-29 09:41:28 +0000

    updated clock styles

--- js/about-us.js
+++ js/about-us.js
@@ -3,3 +3,28 @@ function updateClock () {
 clockContainer.innerText = new Date().toLocaleTimeString();
 }
 setInterval(updateClock, 1000);
+
+const container = document.getElementById('.clock');
+const text = document.getElementById('.clock');
+
+const totalTime = 7500;
+const breatheTime = (totalTime / 5) * 2;
+const holdTime = totalTime / 5;
+
+breathAnimation();
+
+function breathAnimation() {
+  text.innerText = 'Breathe In!';
+  clock.className = '.clock grow';
+
+  setTimeout(() => {
+    text.innerText = 'Hold';
+
+    setTimeout(() => {
+      text.innerText = 'Breathe Out!';
+      clock.className = '.clock shrink';
+    }, holdTime);
+  }, breatheTime);
+}
+
+setInterval(breathAnimation, totalTime);
