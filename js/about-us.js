f62e8c471d50d0c920fe8d9fb934d0d1953bfabc
maria-bilo <marichkabilo@gmail.com>
2022-06-30 17:30:12 +0000

    updated forecast function

--- js/about-us.js
+++ js/about-us.js
@@ -5,19 +5,15 @@ clockContainer.innerText = new Date().toLocaleTimeString();
 setInterval(updateClock, 1000);
 })();
 
-async function () {
+
+async function weatherForecast () {
 const response = await fetch('forecast.json');
 const forecast = await response.json;
-
-}
-renderForecast(forecast);
-}
-
+} 
 document.querySelector('.check-weather').addEventListener('click', weatherForecast);
 
-async function weatherForecast() {
+async function weatherForecast () {
     const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=Oslo&appid=9049f90db361271300a108febe8ed053');
     const forecastNorway = await response.json();
-    const cityName = document.querySelector(.weather-input-value).value;
-    renderForecast(forecast);
+    const cityName = document.querySelector('.weather-input-value').value;
   }
