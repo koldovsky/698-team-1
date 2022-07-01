8f6338105193e2ded766890b09ef65c1d9d4b31a
maria-bilo <marichkabilo@gmail.com>
2022-07-01 06:12:18 +0000

    updated weather forecast

--- js/weather-forecast.js
+++ js/weather-forecast.js
@@ -1,3 +1,39 @@
+async function getWeather () {
+    const response = await fetch('forecast.json');
+    const forecast = await response.json;
+    } 
+    document.querySelector('.check-weather').addEventListener('click', weatherForecast);
+
+    (async function weatherForecast () {
+        const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=Oslo&appid=9049f90db361271300a108febe8ed053');
+        const forecastNorway = await response.json();
+      })();
+
+/*
+async function weatherForecast(name) 
+{
+  let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q={name}&appid=9049f90db361271300a108febe8ed053
+  `);
+  let data = await response.json()
+  return data;
+}  
+
+weatherForecast('yourCitynameHere')
+  .then(data => console.log(data)); 
+document.querySelector('.check-weather').addEventListener('click', weatherForecast);
+
+/*
+function weatherForecast() {
+    fetch("forecast.json")
+    .then(response => response.json())
+    .then((data) => {
+        window.open('https://api.openweathermap.org/data/2.5/weather?q=Oslo&appid=9049f90db361271300a108febe8ed053');
+    })
+    .catch((error) => console.error("oops:",error));
+}
+*/
+
+/*
 async function weatherForecast () {
     const response = await fetch('forecast.json');
     const forecast = await response.json;
@@ -9,7 +45,7 @@ async function weatherForecast () {
         const forecastNorway = await response.json();
         const cityName = document.querySelector('.weather-input-value').value;
       })();
-    
+    */
     
     /*
       async function getWeather(){
